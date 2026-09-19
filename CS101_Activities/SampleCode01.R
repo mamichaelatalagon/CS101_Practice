b51b9ab9cfaa7647dfc2e512189acdccbad7858d
Ma. Michaela Talagon <ma.michaela.talagon@students.isatu.edu.ph>
2026-09-19 12:56:33 +0000

    Assignment

---
+++ CS101_Activities/SampleCode01.R
@@ -0,0 +1,22 @@
+# Assignment 
+
+header1 <- "Assignment - First R Script"
+name <- "Ma. Michaela G. Talagon"
+course <- "BSIT"
+yr_section <- "2B"
+
+# Conceptual Questions
+# No.1: What is the filename extension of the R script?
+# No.2: What is the keyboard shortcut to execute the current line of code?
+
+header2 <- "Conceptual Answers"
+answer1 <- "a) .R"
+answer2 <- "b) Ctrl + Enter"
+
+print(header1)
+print(name)
+print(course)
+print(yr_section)
+print(header2)
+print(answer1)
+print(answer2)
